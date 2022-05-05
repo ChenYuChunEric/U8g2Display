563cc5d6926c2fff0cfaba156d6c5a0098783a47
ChenYuChunEric <88543290+ChenYuChunEric@users.noreply.github.com>
2022-05-05 20:18:54 +0800

    Add files via upload

---
+++ ESP32_U8g2_SSD1306_Switch3Item/ESP32_U8g2_SSD1306_Switch3Item2-2/ESP32_U8g2_SSD1306_Switch3Item2-2.ino
@@ -0,0 +1,80 @@
+/*
+ * Thx for using and sharing the code
+ * Hope it can help you figure uot something new
+ * If so, don't forget to share with others
+ * Enjoy in Arduino, Love & Peace
+ * FB     : https://www.facebook.com/EricChenYuChun/
+ * GitHub : https://github.com/ChenYuChunEric?tab=repositories
+ * 
+ * Made by Yu Chun Chen (EricChen)  2022.03
+ */
+
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+int LB = 25;
+int RB = 26;
+unsigned int count = 0;
+
+void setup() {
+  u8g2.begin();
+  u8g2.enableUTF8Print();
+  pinMode(LB,INPUT);
+  pinMode(RB,INPUT);
+}
+
+void loop() {
+  if(digitalRead(LB) == HIGH){
+    count--;
+    delay(20);
+  }
+  if(digitalRead(RB) == HIGH){
+    count++;
+    delay(20);
+  }
+  
+  if(count%3 == 0){
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_luBS18_tf); //設定字型
+    u8g2.firstPage();
+    do {
+      u8g2.setCursor(44, 18);
+      u8g2.print("Item 1");
+      u8g2.setCursor(44, 41);
+      u8g2.print("Item 2");
+      u8g2.setCursor(44, 64);
+      u8g2.print("Item 3");
+      u8g2.drawFrame(0,0,128,18);
+    } while ( u8g2.nextPage() );
+  }
+  if(count%3 == 1){
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_luBS18_tf); //設定字型
+    u8g2.firstPage();
+    do {
+      u8g2.setCursor(44, 64);
+      u8g2.print("Item 1");
+      u8g2.setCursor(44, 18);
+      u8g2.print("Item 2");
+      u8g2.setCursor(44, 41);
+      u8g2.print("Item 3");
+      u8g2.drawFrame(0,0,128,18);
+    } while ( u8g2.nextPage() );
+  }
+  if(count%3 == 2){
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_luBS18_tf); //設定字型
+    u8g2.firstPage();
+    do {
+      u8g2.setCursor(44, 41);
+      u8g2.print("Item 1");
+      u8g2.setCursor(44, 64);
+      u8g2.print("Item 2");
+      u8g2.setCursor(44, 18);
+      u8g2.print("Item 3");
+      u8g2.drawFrame(0,0,128,18);
+    } while ( u8g2.nextPage() );
+  }
+}
